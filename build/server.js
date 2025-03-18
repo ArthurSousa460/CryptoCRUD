dbfb321953ba75d0bc93774326827bc3a4864a19
ArthuSousa460 <arthurprogrammer460@gmail.com>
2025-03-18 15:57:44 -0300

    refactore .gitignore

--- build/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import router from './routes.js';
-const app = express();
-app.use(express.json());
-app.use(router);
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
